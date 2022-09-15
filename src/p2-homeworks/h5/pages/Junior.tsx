1a9b73173773da221054e4efdcafffb6e531e4c4
Anna-Mkr <annamk91@mail.ru>
2022-09-15 17:49:13 +0300

    connecting and refactoring new components

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Junior() {
+    return (
+        <div>
+            Junior
+        </div>
+    )
+}
+export default Junior
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            Junior plus
+        </div>
+    )
+}
+export default JuniorPlus
