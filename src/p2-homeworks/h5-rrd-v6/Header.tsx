b80509ed17783658fcacaeb4f6f1195a5aa3ff9e
Anna-Mkr <annamk91@mail.ru>
2022-09-17 22:46:41 +0300

    Add news pages in header. Refactoring adding styles

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from "./Header.module.css"
+
+type SetActiveType = (props: { isActive: boolean; }) => string | undefined
 
 function Header() {
+    const setActive: SetActiveType = ({isActive}) => isActive ? s.active : s.notActive
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.wrapper}>
+            <NavLink style={{textDecoration: 'none'}} className={setActive} to={PATH.PRE_JUNIOR}> PreJunior </NavLink>
+            <NavLink style={{textDecoration: 'none'}} className={setActive} to={PATH.JUNIOR}> Junior </NavLink>
+            <NavLink style={{textDecoration: 'none'}} className={setActive} to={PATH.JUNIOR_PLUS}> JuniorPlus </NavLink>
+            <img className={s.NavigationIcon} alt={"Navigations icon"} src={"https://img.icons8.com/nolan/344/compass.png"}/>
         </div>
     )
 }
