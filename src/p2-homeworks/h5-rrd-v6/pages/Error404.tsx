e4c8864fd460516cde0d7b3483af7f694b0a529f
Anna-Mkr <annamk91@mail.ru>
2022-09-17 22:55:34 +0300

    refactoring

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,17 +1,5 @@
 import React from 'react'
 import './Error404.css'
-//
-// function Error404() {
-//     return (
-//         <div>
-//             <div>404</div>
-//             <div>Page not found!</div>
-//             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
-//         </div>
-//     )
-// }
-//
-// export default Error404
 
 function Error404() {
     return (
