41b1d2364991194fef449e10083c23b307375e81
Anna-Mkr <annamk91@mail.ru>
2022-09-17 22:47:46 +0300

    refactoring

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import Header from './Header'
 import Pages from './Pages'
+import {HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
+                <div>
+                    <Header/>
+                </div>
 
-            <Header/>
+                <div>
+                    <Pages/>
+                </div>
 
-            <Pages/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
